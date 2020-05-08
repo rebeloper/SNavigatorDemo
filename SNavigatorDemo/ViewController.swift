a8be939de809eee31fc3c04c4bf5c5204cb1b57a
Alex Nagy <therebeloper@gmail.com>
2020-05-08 09:26:03 +0300

    Initial Commit

---
+++ SNavigatorDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SNavigatorDemo
+//
+//  Created by Alex Nagy on 08/05/2020.
+//  Copyright © 2020 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
